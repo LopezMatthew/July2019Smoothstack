4ee3c5f7a39ce89fff98b4850ade065c83ae8efb
LopezMatthew <mlonius@gmail.com>
2019-07-31 11:46:01 -0400

    Homework2
    A program to create a two dimesnional array and find the largest number within that array, a program to sum up a number of integers based on user input, and a conceptual framework demonstrating the relationship between interfaces and classes that implement it.

---
+++ Homework2/Shape.java
@@ -0,0 +1,27 @@
+
+public interface Shape {
+public void calculateArea();
+public void display();
+}
+
+class circle implements Shape{
+	public void calculateArea(){
+	}
+	public void display() {
+		
+	}
+}
+class square implements Shape{
+	public void calculateArea(){
+	}
+	public void display() {
+		
+	}
+}
+class rectangle implements Shape{
+	public void calculateArea(){
+	}
+	public void display() {
+		
+	}
+}
